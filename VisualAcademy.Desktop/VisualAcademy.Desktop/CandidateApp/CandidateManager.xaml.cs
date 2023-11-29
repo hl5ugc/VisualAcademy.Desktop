d22e0d83a2cb9372049d2b7ddbea613e824a2c4a
hl5ugc <hl5ugc@nate.com>
2023-11-29 14:44:25 +0900

    01

---
+++ VisualAcademy.Desktop/VisualAcademy.Desktop/CandidateApp/CandidateManager.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace VisualAcademy.Desktop.CandidateApp
+{
+    /// <summary>
+    /// CandidateManager.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class CandidateManager : Window
+    {
+        public CandidateManager()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddCandidate_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("직원 후보자 추가");
+            btnAddCandidate.Content = $"직원 후보자 추가 완료 {DateTime.Now.Second}";
+        }
+    }
+}
---
+++ VisualAcademy.Desktop/VisualAcademy.Desktop/Demos/FrmContextMenu.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VisualAcademy.Desktop.Demos
+{
+    /// <summary>
+    /// FrmContextMenu.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class FrmContextMenu : Window
+    {
+        public FrmContextMenu()
+        {
+            InitializeComponent();
+            miSecond.Click += new RoutedEventHandler(miSecond_Click);
+            miThird.Click += (s, e) => { MessageBox.Show("세번째 "); };
+        }
+
+        private void miFisrt_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("첫번째 메뉴 클릭됨");
+        }
+
+        private void miSecond_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("두번째 메뉴 클릭됨");
+            if(miFisrt.IsChecked == true)
+            {
+                miThird.IsEnabled = true;
+                miFisrt.IsChecked = false;
+
+            }
+            else
+            {
+                miThird.IsEnabled = false;
+            }
+             
+        }
+ 
+    }
+}
