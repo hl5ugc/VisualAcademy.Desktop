502a6ea4a9f1fc9591132be53b44b917512bd72d
hl5ugc <hl5ugc@nate.com>
2023-11-29 11:10:24 +0900

    FrmHelloWorld 작성 및  컨트롤 , 이벤트 처리 및 사용

---
+++ VisualAcademy.Desktop/VisualAcademy.Desktop/Demos/FrmHelloworld.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace VisualAcademy.Desktop.Demos
+{
+    /// <summary>
+    /// FrmHelloworld.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class FrmHelloworld : Window
+    {
+        public FrmHelloworld()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDisplay.Text == "")
+            {
+                txtDisplay.Text = $"안녕 하세요 {DateTime.Now.Second}";
+            }
+            else
+            {
+                txtDisplay.Text = string.Empty;
+            }
+        }
+    }
+}
